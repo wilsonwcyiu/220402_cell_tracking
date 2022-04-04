cc0ad0dd19823051e665e63ca207e7d47e81460a
Wilson Yiu <wilson.yiu@mail.digiec.com>
2022-04-04 20:50:30 +0200

    demo

---
+++ test_case/test_demo.py
@@ -0,0 +1,68 @@
+from os import listdir
+
+from main.viterbi_adjust2_enhancement import find_existing_series_list
+
+if __name__ == '__main__':
+    folder_path: str = 'D:/viterbi linkage/dataset/'
+    output_folder = folder_path + 'output_unet_seg_finetune//'
+
+    # test case 1
+    input_series_list = ['S01']
+    series_list = find_existing_series_list(input_series_list, listdir(output_folder))
+    # print(series_list)
+    if series_list == ['S01']:
+        print("pass")
+    else:
+        print("failed")
+
+
+    # test case 2
+    input_series_list = ['S30']
+    series_list = find_existing_series_list(input_series_list, listdir(output_folder))
+
+    if series_list == []:
+        print("pass")
+    else:
+        print("failed")
+
+
+    # test case 3
+    input_series_list = ['S02', 'S34']
+    series_list = find_existing_series_list(input_series_list, listdir(output_folder))
+
+    if series_list == ['S02']:
+        print("pass")
+    else:
+        print("failed")
+
+
+    # test case 4
+    input_series_list = []
+    series_list = find_existing_series_list(input_series_list, listdir(output_folder))
+
+    if series_list == []:
+        print("pass")
+    else:
+        print("failed")
+
+
+
+    # test case 5
+    input_series_list = None
+    series_list = find_existing_series_list(input_series_list, listdir(output_folder))
+
+    if series_list == []:
+        print("pass")
+    else:
+        print("failed")
+
+
+    # test case 6
+    input_series_list = None
+    output_folder = None
+    series_list = find_existing_series_list(input_series_list, output_folder)
+
+    if series_list == []:
+        print("pass")
+    else:
+        print("failed")
